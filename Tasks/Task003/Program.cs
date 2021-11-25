116af332d4d3367b30af91abec19b757def51229
BentecMike <kipsmv@gmail.com>
2021-11-25 21:40:25 +0500

    add Task 003

---
+++ Tasks/Task003/Program.cs
@@ -0,0 +1,11 @@
+﻿//Даны два числа. Показать большее и меньшее число
+int number1 = new Random().Next(1, 20);
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 20);
+Console.WriteLine(number2);
+
+
+if (number1 > number2)
+    Console.WriteLine("Число 1 больше числа 2");
+else
+    Console.WriteLine("Число 2 больше числа 1");
