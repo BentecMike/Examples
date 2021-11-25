36f499b643dae909763430fb2119b663f5f88e3e
BentecMike <kipsmv@gmail.com>
2021-11-25 23:31:37 +0500

    add task 005

---
+++ Tasks/Task005/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимальное из трех чисел
+int numa = new Random().Next(1, 1000);
+Console.WriteLine(numa);
+int numb = new Random().Next(1, 1000);
+Console.WriteLine(numb);
+int numc = new Random().Next(1, 1000);
+Console.WriteLine(numc);
+
+int max = numa;
+
+if (numa > max) max = numa;
+if (numb > max) max = numb;
+if (numc > max) max = numc;
+Console.WriteLine(max);
