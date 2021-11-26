c8f9d6746417fc038e2885fda7cf4f4037867020
BentecMike <kipsmv@gmail.com>
2021-11-26 12:05:46 +0500

    add Task 006 y = f(a)

---
+++ Tasks/Task006/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу вычисления значения функции y = f(a) y = a^3 * 2 + 15
+
+double a = new Random().Next (1, 10);
+Console.WriteLine(a);
+double y = Math.Pow (a, 3) * 2 + 15;
+
+double f(double a, double y)
+{
+    y = a * a * a * 2 + 15;
+    return y;
+}
+
+Console.WriteLine(y);
+
