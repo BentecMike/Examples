34e273dc18c57f33e5b9c481eb46be2d75577494
BentecMike <kipsmv@gmail.com>
2021-11-26 12:13:05 +0500

    add Task 007 Четное нечетное

---
+++ Tasks/Task007/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным
+
+int a = new Random().Next(1, 35);
+Console.WriteLine(a);
+
+int b = a % 2;
+if (b == 0)
+    Console.WriteLine("Четное");
+else
+    Console.WriteLine("Нечетное");
