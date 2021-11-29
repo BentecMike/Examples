19affcbf75ee778ba2a723cfe7bb469f0a1f4026
BentecMike <kipsmv@gmail.com>
2021-11-29 18:46:52 +0500

    add task 008 Показать числа от -N до N

---
+++ Tasks/Task008/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+int[] array = new int[5000000];
+
+//int[] array = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int count = array.Length;
+for (int i = 0; i < array.Length - 1; i++)
+{
+    array[i] = new Random().Next(-2014856, 25541617);
+    Console.Write($"{array[i]} ");
+}
