c20dd9988637e478a2f9c7b49988df688ced3421
BentecMike <kipsmv@gmail.com>
2021-11-29 21:52:54 +0500

    add Task 013 Удалить вторую цифру трехзначного числа

---
+++ Tasks/Task013/Program.cs
@@ -0,0 +1,12 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int a = Convert.ToInt32(new Random().Next (100, 999));
+ Console.WriteLine(a);
+     Console.WriteLine(a % 10);
+     int a3 = a % 10;
+     Console.WriteLine(a % 100/10);
+     int a2 = a % 100/10;
+     Console.WriteLine(a % 1000/100);
+     int a1 = a % 1000/100;
+     Console.Write(a1);
+    Console.Write(a3);
