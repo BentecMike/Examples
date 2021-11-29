a921adc0f314ef01f5ff64a3e7fd856688db2dbc
BentecMike <kipsmv@gmail.com>
2021-11-29 21:39:50 +0500

    add Task 012 Отрезок (10,99). показать max цифру числа

---
+++ Tasks/Task012/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next (10, 99);
+Console.WriteLine(a);
+
+int a1 = a % 10;
+Console.WriteLine(a1);
+int a2 = a % 100/10;
+Console.WriteLine(a2);
+if (a1 > a2)
+Console.WriteLine("Наибольшее число из двух: " + a1);
+else 
+Console.WriteLine("Наибольшее число из двух: " + a2);
