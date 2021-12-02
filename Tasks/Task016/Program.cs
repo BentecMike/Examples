6339e13fe9dd1107813307190116e5d62164eca6
BentecMike <kipsmv@gmail.com>
2021-12-02 11:32:12 +0500

    add Task 016 Выяснить является номер дня недели выходным

---
+++ Tasks/Task016/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+int day = new Random().Next(1, 8); //ноябрь 2021 - 30 дней
+Console.WriteLine(day);
+
+if (day == 7 || day == 6)
+    Console.WriteLine("Сегодня выходной день");
+else
+    Console.WriteLine("Сегодня будний день");
