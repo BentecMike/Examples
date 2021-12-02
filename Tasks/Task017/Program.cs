f7a0d102d4db4afcd464bd395b2772fb66f19356
BentecMike <kipsmv@gmail.com>
2021-12-02 11:42:28 +0500

    add Task 017 По двум заданным числам проверять является ли одно квадратом другого

---
+++ Tasks/Task017/Program.cs
@@ -0,0 +1,11 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+int a = new Random().Next (4, 26);
+Console.WriteLine(a);
+int b = new Random().Next (1, 6);
+Console.WriteLine(b);
+
+if (a == Math.Pow (b, 2))
+Console.WriteLine($"{a}{" является квадратом"} {b}");
+else
+Console.WriteLine($"{a}{" не является квадратом"} {b}");
