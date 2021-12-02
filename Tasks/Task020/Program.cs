d5b3bb3f43bf6b295c166851b2cc261515d76389
BentecMike <kipsmv@gmail.com>
2021-12-02 18:29:11 +0500

    add Task 020 Задать ночер четверти, показать диапазоны для возможных координат

---
+++ Tasks/Task020/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Введите номер четвети (0, 1, 2, 3, 4): ");
+int q = new Random().Next(0, 5);
+Console.WriteLine(q);
+
+if (q == 1)
+Console.WriteLine("x > 0 и у > 0");
+if (q == 2)
+Console.WriteLine("x < 0 и y > 0");
+if (q == 3)
+Console.WriteLine("x < 0 и y < 0");
+if (q == 4)
+Console.WriteLine("x > 0 и y < 0");
+if (q == 0)
+Console.WriteLine("x = 0 и y = 0");
---
+++ Tasks/Task021/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа проверяет пятизначное число на палиндромом
+
+Console.Write("Введите пятизначное число: ");
+string q = Console.ReadLine();
+int a = int.Parse(q[0].ToString());
+int b = int.Parse(q[1].ToString());
+int c = int.Parse(q[2].ToString());
+int d = int.Parse(q[3].ToString());
+int e = int.Parse(q[4].ToString());
+
+if (a == e && b == d)
+Console.WriteLine($"{q} {"является палиндромом"}");
+else
+Console.WriteLine($"{q} {"не является палиндромом"}");
