5865da29cabbbb67507518ce32d748339fcb935f
BentecMike <kipsmv@gmail.com>
2021-12-02 16:58:47 +0500

    add Task 019 Определить номер четверти плоскости

---
+++ Tasks/Task019/Program.cs
@@ -0,0 +1,15 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int x = -5;
+int y = -6;
+
+if (x > 0 && y > 0)
+Console.WriteLine("I четверть плоскости");
+if (x < 0 && y > 0)
+Console.WriteLine("II четверть плоскости");
+if (x < 0 && y < 0)
+Console.WriteLine("III четверть плоскости");
+if (x > 0 && y < 0)
+Console.WriteLine("IV четверть плоскости");
+else 
+Console.WriteLine("Точка начала координат");
