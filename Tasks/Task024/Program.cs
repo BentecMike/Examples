6e7f8248af5dc20f3628ad302de3908a5dec585a
BentecMike <kipsmv@gmail.com>
2021-12-03 16:39:28 +0500

    add Task 024 Найти кубы чисел от 1 до N

---
+++ Tasks/Task024/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти кубы чисел от 1 до N (N=20)
+
+int a;
+int N = 20;
+
+static int Kvadrat (int a)
+    {
+        return a*a*a; 
+    }
+
+for (a = 1; a <= N; a++ )    
+    {
+        Kvadrat(a);
+        Console.WriteLine($"{a}        {a*a*a}");
+        
+    }
