55d5ada90799f0e6957f9a00dafa00811baa3c7b
BentecMike <kipsmv@gmail.com>
2021-12-03 16:33:05 +0500

    add Task 023 Показать таблицу квадратов чисел от 1 до N

---
+++ Tasks/Task023/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать таблицу квадратов чисел от 1 до N 
+int a;
+int N = 20;
+
+static int Kvadrat (int a)
+    {
+        return a*a; 
+    }
+
+for (a = 1; a <= N; a++ )    
+    {
+        Kvadrat(a);
+        Console.WriteLine($"{a}        {a*a}");
+        
+    }
