58c14320bfc33564486ec411b422d47b71c01e85
BentecMike <kipsmv@gmail.com>
2021-12-03 16:10:04 +0500

    add Task 022 Найти расстояние между точками в пространстве

--- Tasks/Task022/Program.cs
+++ Tasks/Task022/Program.cs
@@ -1,83 +1,21 @@
-﻿// Найти расстояние между точками в пространстве 2D/3D
-Console.Clear();
-int x1 = new Random().Next(-10, 11);
-Console.Write(x1);
-int y1 = new Random().Next(-10, 11);
-Console.Write(y1);
+﻿// Найти расстояние между точками в пространстве 2D/3D (AB = √(xb - xa)2 + (yb - ya)2) //(AB = √(xb - xa)2 + (yb - ya)2)
 
-int x2 = new Random().Next(-10, 11);
-Console.Write(x2);
-int y2 = new Random().Next(-10, 10);
-Console.Write(y2);
+double x1 = new Random().Next(-10, 11);
+double y1 = new Random().Next(-10, 11);
+//double z1 = new Random().Next(-10, 11);
+Console.WriteLine($"{x1} {y1}");
+//Console.WriteLine($"{x1} {y1} {z1}");
 
-int rx;
-int ry;
+double x2 = new Random().Next(-10, 11);
+double y2 = new Random().Next(-10, 10);
+//double z2 = new Random().Next(-10, 11);
+Console.WriteLine($"{x2} {y2}");
+//Console.WriteLine($"{x2} {y2} {z2}");
 
-if (x1 <= x2 && y1 <= y2)
-    {
-        rx = 0;
-
-    }
-
-/*if (x1 == x2 && y1 != y2)
-  {  rx = 0;
-     ry = Math.Abs(y1) + Math.Abs(y2);
-    Console.WriteLine($"{"Расстояние между точками x ="} {rx} {"y ="} {ry}");
-  }
-if (x1 != x2 && y1 == y2)
-  {  rx = Math.Abs(x1) + Math.Abs(x2);;
-     ry = 0;
-    Console.WriteLine($"{"Расстояние между точками x ="} {rx} {"y ="} {ry}");
-  }
-else
-    rx = Math.Abs(x1) + Math.Abs(x2);
-    ry = Math.Abs(y1) + Math.Abs(y2);
-    Console.WriteLine($"{"Расстояние между точками x ="} {rx} {"y ="} {ry}");
-    */
-/*Console.Clear();
-//Console.SetCursorPosition(10, 4);
-//Console.WriteLine("+");
-
-int xa = 40, ya = 1,
-    xb = 1, yb = 30,
-    xc = 80, yc = 30;
-
-Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
-
-Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
-
-Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
-
-
-int x = xa, y = xb;
-
-int count = 0;
-
-while (count < 2000)
-{
-    int what = new Random().Next(0, 3); //[0;3) 0 1 2
-    if (what == 0)
-    {
-        x = (x + xa) / 2;
-        y = (y + ya) / 2;
-    }
-    if (what == 1)
-    {
-        x = (x + xb) / 2;
-        y = (y + yb) / 2;
-    }
-    if (what == 2)
-    {
-        x = (x + xc) / 2;
-        y = (y + yc) / 2;
-    }
-
-    Console.SetCursorPosition(x, y);
-    Console.WriteLine("+");
-    count ++;
-
-}
-*/
+double x = Math.Pow((x2-x1),2);
+double y = Math.Pow((y2-y1),2); 
+//double z = Math.Pow((z2-z1),2); 
+double XY = Math.Sqrt(x + y);
+//double XYZ = Math.Sqrt(x + y + z);
+Console.WriteLine(XY);
+//Console.WriteLine(XYZ);
