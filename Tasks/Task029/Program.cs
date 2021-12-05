a182ed512e372a215204b4429bbfe2211982cd7a
BentecMike <kipsmv@gmail.com>
2021-12-05 21:17:57 +0500

    add Task 029 Написать программу вычисления произведения чисел от 1 до N

---
+++ Tasks/Task029/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+double N = int.Parse(Console.ReadLine());
+double result = 1;
+for (int i = 1; i <= N; i++)
+{
+	result *= i;
+}
+Console.WriteLine(result);
