7777ed4dc0ea41f2708f1fe33a01e2e3f230b885
BentecMike <kipsmv@gmail.com>
2021-12-05 20:20:37 +0500

    add Task 027 Определить количество цифр в числе

---
+++ Tasks/Task027/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine($"Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int i = 0;
+int result;
+while (A > 0)
+    {
+       result = A /= 10; 
+       i++;
+    }              
+Console.WriteLine(i);
