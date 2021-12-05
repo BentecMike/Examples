b8fef03ef166bc3bd1a64f960743808e39fb9b7f
BentecMike <kipsmv@gmail.com>
2021-12-05 19:22:45 +0500

    add Task 026 Возведите число в натуральную степень используя цикл

---
+++ Tasks/Task026/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+Console.WriteLine($"Введите число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"Введите степень: ");
+int B = int.Parse(Console.ReadLine());
+int prom = 1;
+for (int i = 0; i < B; i++)
+{
+    prom *= A;
+    if (i == B) ;
+    Console.WriteLine(prom);
+
+}
