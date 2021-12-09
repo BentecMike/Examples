4449f2decc81de6c494715b85598e50e32d77e8d
BentecMike <kipsmv@gmail.com>
2021-12-09 22:00:48 +0500

    add Task 034 Написать программу замену элементов массива на противоположные

---
+++ Tasks/Task034/Program.cs
@@ -0,0 +1,42 @@
+﻿// Написать программу замену элементов массива на противоположные
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-9, 10);
+        index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+void PrintArrayNew(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]*-1);
+        position++;
+    }
+}
+
+int[] array = new int[12];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("now new array");
+PrintArrayNew(array);
