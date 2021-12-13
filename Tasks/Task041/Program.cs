5e5e476c5f0317b616dd5812bbc27b49e539f3bc
BentecMike <kipsmv@gmail.com>
2021-12-13 18:23:03 +0500

    add Task 041 выяснить являются ли три числа сторонами треугольника

---
+++ Tasks/Task041/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);
+int c = new Random().Next(1, 100);
+Console.WriteLine($"{a}, {b}, {c}");
+
+if (a < b + c && b < a + c && c < a + b)
+    {
+        Console.WriteLine($"Числа {a}, {b}, {c} являются сторонами треугольника");
+    }
+else 
+    {
+        Console.WriteLine($"Числа {a}, {b}, {c} НЕявляются сторонами треугольника");
+    }
