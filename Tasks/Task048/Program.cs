e07049d04cdddd4dccf7e709aacecffe4555ecef
BentecMike <kipsmv@gmail.com>
2021-12-16 22:54:30 +0500

    add Task 048 Показать двумерный массив m*n

---
+++ Tasks/Task048/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать двумерный массив размером m×n заполненный целыми числами
+
+int[,] mass = {{1, 2, 3, 4}, {5, 6, 7, 8}};
+
+for (int i = 0; i < mass.GetLength(0); i++)
+    {   
+         for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write("{0,3}", mass[i, j]);
+                }
+                Console.WriteLine();
+    }
