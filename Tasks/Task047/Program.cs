b6efd7666cefaad110ce1f1e1960766a544d38b5
BentecMike <kipsmv@gmail.com>
2021-12-16 22:35:00 +0500

    add Task 047 Написать программу копирования массива

---
+++ Tasks/Task047/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу копирования массива
+
+int [] array = {1, 2, 5, 8, 6};
+
+int [] anotherArray = new int[5];
+
+Array.Copy(array, anotherArray, array.Length); //Метод копирования массива
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i] + "-" + anotherArray[i]);
+}
+
+anotherArray[3] = 7;
+
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine(array[i] + "-" + anotherArray[i]);
+}
