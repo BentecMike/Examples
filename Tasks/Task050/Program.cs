58a0caef4360f26927bd50cd2fea6df07622af58
BentecMike <kipsmv@gmail.com>
2021-12-18 11:29:45 +0500

    add Task 051 Задать двумерный массив Аmn = m+n

---
+++ Tasks/Task050/Program.cs
@@ -0,0 +1,43 @@
+﻿// В двумерном массиве n×k заменить четные элементы на противоположные
+
+
+double[,] mass = new double[4, 4];    
+Random random = new Random();
+for (int i = 0; i < mass.GetLength(0); i++)
+    {   
+         for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    mass[i, j] = random.Next(-100,100) / 10.00;
+                    Console.Write("{0,15}", mass[i, j]);
+                }
+                Console.WriteLine();
+    }
+
+for (int i = 0; i < mass.GetLength(0); i++)
+    {   
+         for (int j = 0; j < mass.GetLength(1); j++)
+                {   
+                    Console.WriteLine(mass[i, j] % 2 + " ");
+                    if (mass[i, j] % 2 < 0.000000000000001 && mass[i,j] % 2)
+                        {
+                            mass[i,j] = -mass[i,j];
+                        } 
+                
+                }
+    }
+Console.WriteLine();
+
+void PrintArray (double [,] collection)
+{
+    for(int i = 0; i < collection.GetLength(0); i++)
+    {
+        for(int j = 0; j < collection.GetLength(1); j++)
+        {
+            Console.Write("{0,15}", collection[i, j]);
+            //Console.Write($"{collection[i, j]}  ");
+        }
+    Console.WriteLine();
+    }
+}
+
+PrintArray(mass);
---
+++ Tasks/Task051/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+double[,] mass = new double[4, 4];    
+Random random = new Random();
+for (int i = 0; i < mass.GetLength(0); i++)
+    {   
+         for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    mass[i, j] = random.Next(-100,100);
+                    Console.Write("{0,15}", mass[i, j]);
+                }
+                Console.WriteLine();
+    }
+
+for (int i = 0; i < mass.GetLength(0); i++)
+    {   
+         for (int j = 0; j < mass.GetLength(1); j++)
+                {   
+                    mass[i,j] = i + j;
+
+                
+                }
+    }
+Console.WriteLine();
+
+void PrintArray (double [,] collection)
+{
+    for(int i = 0; i < collection.GetLength(0); i++)
+    {
+        for(int j = 0; j < collection.GetLength(1); j++)
+        {
+            Console.Write("{0,15}", collection[i, j]);
+            //Console.Write($"{collection[i, j]}  ");
+        }
+    Console.WriteLine();
+    }
+}
+
+PrintArray(mass);
